20a6aa1bcca3c045207b380b2688c0b164415b0b
Bloomle <louishoff83@gmail.com>
2025-11-13 14:22:49 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -60,11 +60,12 @@ export default function Home() {
           >
             Ich bringe 3 Personen mit
           </button>
-              <a>
-                href="/admin"
-                className="mt-6 inline-block text-sm text-gray-500 hover:text-gray-700 underline"
-                <!--Admin Login-->
-              </a>
+              <a
+  href="/admin"
+  className="mt-6 inline-block text-sm text-gray-500 hover:text-gray-700 underline"
+>
+  Admin Login
+</a>
         </div>
       )}
 
